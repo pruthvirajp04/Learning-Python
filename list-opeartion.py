a4977607284203c48a1196e3b22c6966bb14bbdd
Pruthviraj <pruthviraj.p@somaiya.edu>
2022-02-20 15:12:34 +0530

    two more files

---
+++ list-opeartion.py
@@ -0,0 +1,12 @@
+mylist = ["Pruthviraj", "Aditya", "Vedant","Michael"]
+print("Now 'Kartik' would be appended")
+mylist.append("Kartik")
+print(mylist)
+print("Reversed list :")
+mylist.reverse()
+print(mylist)
+print("Removing or poping the 4th element")
+mylist.pop(3)
+print(mylist)
+print("The length of the list is : ")
+print(len(mylist)) 
---
+++ tuple-list.py
@@ -0,0 +1,8 @@
+tuple1 = ("Pruthvi","Jordan","Tiger","Yamuna")
+print("The 4th element :",tuple1[3])
+myList = list(tuple1)
+print(myList)
+if 'Pruthvi' in myList:
+    print("Pruthvi is present in the list")
+else:
+    print("Pruthvi is not present in the list")
